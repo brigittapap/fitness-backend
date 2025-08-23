e4fa512f7cf11bce2f6c676d56a2af7ef612be57
Brigitta  Pap <pap.mbrigitta@gmail.com>
2025-08-23 17:04:40 +0300

    Initial commit: Spring Boot skeleton with Flyway V1

---
+++ src/test/java/com/brigi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
